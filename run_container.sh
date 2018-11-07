1f8654155c26b522f6053a75f9abf5b1e1314594
Nina Maller <nina.maller@gmail.com>
2018-11-07 15:04:18 -0800

    Update run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -4,10 +4,10 @@ printf "generating base image..."
 ./generate-base-image/scripts/build_docker.sh
 
 printf "\nbuild demo image..."
-docker build -t docker-hibench-demo /home/debug-hibench/docker-hibench-demo
+docker build -t docker-hibench-demo ./docker-hibench-demo
 
 #run the container
 printf "\nRunning the container\n"
-#docker run -it --rm docker-hibench-demo
+docker run -it --rm docker-hibench-demo
 #gotty-dir/gotty --config gotty-dir/gotty.cfg --max-connection 10 docker run --net isolated_nw -it --rm docker-hibench-demo &> /dev/null &
 
